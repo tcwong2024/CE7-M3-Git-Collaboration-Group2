3bd9dc95ad37030ed44350c7d0e4e0a5075a8269
tcwong2024 <tcwong99@hotmail.com>
2024-10-10 17:39:20 +0800

    Update storage.tf to create resource for acl

--- storage.tf
+++ storage.tf
@@ -1,9 +1,14 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.s3_bucket_name
-  acl    = "private"
 
   tags = {
     Name        = var.s3_bucket_name
     Environment = var.environment
   }
+}
+
+# resource for ACL
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "private"
 }
