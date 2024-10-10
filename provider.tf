0936b8ab74521590ae30e02801fafb0dd1bb7b68
tcwong2024 <tcwong99@hotmail.com>
2024-10-10 12:58:53 +0800

    Add archive and require version in provider

--- provider.tf
+++ provider.tf
@@ -4,7 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    # Added by wtc - Move archive provider from lambda.tf
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
+    }
   }
+  # Added by wtc 
+  required_version = ">= 1.1.0"
 }
 
 # Configure the AWS Provider
