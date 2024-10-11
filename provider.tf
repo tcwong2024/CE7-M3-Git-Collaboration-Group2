02ec7f7969100dae3b1bb820fbabb3d010131cf8
tcwong2024 <tcwong99@hotmail.com>
2024-10-11 11:56:00 +0800

    Modify provider.tf change hashicorp v5.70.0

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      # version = "~> 5.0"
+      version = "~> 5.70.0"
+    }
+    # Added by wtc - Move archive provider from lambda.tf
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
+    }
+  }
+  # Added by wtc 
+  required_version = ">= 1.1.0"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  # Added by WTC
+  region = var.region
+}
